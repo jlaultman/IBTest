19e75fe712e92f399125f6ac29bd25761ad295ec
jlaultman <joeyaultman@gmail.com>
2016-03-19 11:13:15 -0400

    Initial Commit

---
+++ IB Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IB Test
+//
+//  Created by Joseph Aultman on 3/19/16.
+//  Copyright © 2016 Joseph Aultman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
